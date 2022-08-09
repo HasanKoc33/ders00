e83fddd3f70f910f16ffc9249906af2a0281d7fd
Hasan <developer.hasankoc+github@gmail.com>
2022-08-09 09:20:08 +0300

    .

---
+++ android/app/src/main/kotlin/com/example/ders00/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ders00
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+
+
+import 'dart:convert';
+import 'dart:io';
+
+void main(){
+  String ogrenci = "50";
+  var a = 'a';
+  int yas = 23;
+  double price = 23.23;
+  bool mantiksal = false;
+
+
+  var hesap1 = yas*price;
+
+ /* var sayi = "hasan";
+  //print(line);
+  if(line==sayi){
+    print("Bu ifade dogru");
+
+  }else{
+print("Bu ifade yanlış");
+  }
+
+  var toplam =0;
+  for(int i=0; i<100; i+=2){
+    toplam += i;
+   // print("Döngü : $i");
+  }
+
+  var hak =3;
+  while(true){
+    print("bir text girin");
+    var line = stdin.readLineSync(encoding: utf8);
+    if(line == "hasan"){
+      print("tebrikler");
+      break;
+    }else {
+      hak--;
+      print("bilemedin");
+    }
+    if(hak==0){
+      print("başarısıxz");
+    }else if(hak ==1){
+      print("son hakkın iyi düşün");
+
+    }
+  }
+
+  */
+
+String isim = "yağız";
+String yasim ="19";
+//yagız19191919...
+  var toplamYas = "";
+for(int i=0; i<1000000; i++){
+  print(i);
+}
+print(isim+toplamYas);
+
+// tip adı = degeri ;
+
+// for( bagılDegişken , çalışma koşulu , arttırma degeri)
+}
